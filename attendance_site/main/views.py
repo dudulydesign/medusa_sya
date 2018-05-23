afa997e3edc17276c8a8709f70bd8a1382f4c881
LAPTOP-H8OBHBKA\Faye <faye.mgproject@gmail.com>
2018-05-23 11:13:34 +0800

    new

--- attendance_site/main/views.py
+++ attendance_site/main/views.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+  print "=>" * 100
+  print "login_view"
+  if request.method == "POST":
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      print "is_valid!!!"
+      user = form.user
+      auth_login(request, user)
+      url2 = reverse_url("index")
+      return HttpResponseRedirect(url2)
+  else:
+    form = LoginForm()
+  return TemplateResponse(request, "login.html", {
+    "login_form": form,
+    })
+def logout_view(request):
+  auth_logout(request)
+  url2 = reverse_url("index")
+  return HttpResponseRedirect(url2)
