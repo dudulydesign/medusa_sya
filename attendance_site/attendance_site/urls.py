f81c926863c6bf802b7ed7561d625edfbf6d70a7
ahui <ahui.mgproject@gmail.com>
2018-05-23 11:28:10 +0800

    save2

--- attendance_site/attendance_site/urls.py
+++ attendance_site/attendance_site/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.static import static 
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import main.urls
@@ -12,4 +13,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
